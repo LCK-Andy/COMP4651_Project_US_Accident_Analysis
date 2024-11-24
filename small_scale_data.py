ea56f520e81b7df471721d4efd5699e99d92e3ed
SW/Andy <li.ck8819@gmail.com>
2024-11-24 23:59:34 +0800

    upload

--- small_scale_data.py
+++ small_scale_data.py
@@ -1,20 +1,31 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import rand
 
 LOCAL_DATA_SOURCE_PATH = "US_Accidents_March23.csv"
 LOCAL_DATA_OUTPUT_PATH = "US_Accidents_March23_small.csv"
 
+
 def main():
+    # Initialize Spark session
     spark = SparkSession.builder.appName("COMP4651Project").getOrCreate()
+
+    # Read the dataset
     df = spark.read.csv(LOCAL_DATA_SOURCE_PATH, header=True, inferSchema=True)
 
-    # Select the first 1000 rows
-    small_df = df.limit(1000)
+    # Shuffle the dataset using orderBy with a random column
+    shuffled_df = df.orderBy(rand())
+
+    # Select the first 5000 rows
+    small_df = shuffled_df.limit(5000)
 
     # Write the small dataframe to a new CSV file
     small_df.write.csv(LOCAL_DATA_OUTPUT_PATH, header=True, mode="overwrite")
 
     print("Small scale data saved to: " + LOCAL_DATA_OUTPUT_PATH)
+
+    # Stop the Spark session
     spark.stop()
+
 
 if __name__ == "__main__":
     main()
